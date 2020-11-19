8648ac4f82e5dc431ebf93a7c636537b7cedf619
mfmcl <michael.mclean@student.leaf.academy>
2020-11-19 12:22:52 +0100

    -

---
+++ APCSA_Homework/src/Point.java
@@ -0,0 +1,17 @@
+package APCSA_Homework.src;
+
+/**
+1.	Define a class Point
+-	Fields: private Int CoordinateX, private Int CoordinateY
+-	Constructors take either non or both (int X, int Y) arguments, where X and Y are from <-100,100> closed interval
+-	Methods:
+o	 private setCoordinates(int, int) will set CoordinateX, CoordinateY
+o	 private getCoordinateX() will return X
+o	 private getCoordinateX() will return Y
+o	toString will print „I am a point with coordinate X: 52, coordinate Y:82“ You can use only getter methods
+o	randomPosition() will set random numbers of coordinates. You can use only Setter methods
+o	 changeX(int) will increase the X coordinate number by the given number
+ */
+public class Point {
+    
+}
